5d74a8b7a5070214083a41246bc135ee338fe704
a2h1r0 <53073064+a2h1r0@users.noreply.github.com>
2023-02-23 17:40:19 +0900

    #3 Create user_service.dart

---
+++ lib/service/user_service.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nomikai/model/user.dart';
+
+class UserService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<List<User>> getUserList() async {
+    // todo: users空の場合の挙動を確認
+    QuerySnapshot snapshot = await _firestore.collection('users').get();
+    List<User> users = snapshot.docs.map((doc) => User(uid: doc.id)).toList();
+
+    return users;
+  }
+}
