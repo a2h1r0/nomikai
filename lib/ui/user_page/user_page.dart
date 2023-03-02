50420127adbd1d9e23e6a1a7d351b82054a4233b
a2h1r0 <53073064+a2h1r0@users.noreply.github.com>
2023-03-03 00:39:09 +0900

    #1 use useFuture function

--- lib/ui/user_page/user_page.dart
+++ lib/ui/user_page/user_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nomikai/model/user.dart';
 import 'package:nomikai/service/user_service.dart';
@@ -9,32 +10,31 @@ class UserPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return FutureBuilder<User?>(
-        future: UserService().getUser(username),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text('読み込み中です．．．',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
-          }
+    final userMemoize = useMemoized(() => UserService().getUser(username));
+    final snapshot = useFuture(userMemoize);
 
-          User? user = snapshot.data;
-          if (user == null) {
-            return const Text('ユーザーが見つかりませんでした．．．',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
-          }
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Text('読み込み中です．．．',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
+    }
 
-          return Scaffold(
-            body: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text('${user.username}のページです',
-                      style: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold)),
-                ],
-              ),
-            ),
-          );
-        });
+    User? user = snapshot.data;
+    if (user == null) {
+      return const Text('ユーザーが見つかりませんでした．．．',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
+    }
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('${user.username}のページです',
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
   }
 }
